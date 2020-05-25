58ffa182b0ed562664316570da03c286a43fc5c7
Michael Reel <mykilr@gmail.com>
2020-05-25 13:44:27 +0100

    Not great this one, might try a different approach

--- FractalRivers.gd
+++ FractalRivers.gd
@@ -1,10 +1,11 @@
 extends TextureRect
 
-const START_WIDTH = 10.0
+const START_WIDTH = 30.0
+const START_ANGLE = 0.0
 const MIN_WIDTH = 0.1
 const LEN_WIDTH_RATIO = 10.0
 const EXTENSION_CHANCE = 0.5
-const ANGLE_CHANGE = PI / 3
+const START_ANGLE_CHANGE = PI / 6
 
 var width : int = rect_size.x
 var height : int = rect_size.y
@@ -12,51 +13,48 @@ var river_mouth : Section
 
 class Section:
 	var width : float
+	var angle : float
 	var source : Vector2
 	var mouth : Section
 	var tributaries : Array
 	
-	func _init(w : float, s : Vector2, m):
+	func _init(w : float, a : float, s : Vector2, m):
 		width = w
+		angle = a
 		source = s
 		mouth = m
 		tributaries = []
-		
-	func get_angle() -> float:
-		return mouth.source.angle_to(source)
 	
 	func draw(canvas : CanvasItem, color : Color):
-		canvas.draw_line(mouth.source, source, color, width)
+		if mouth != null:
+			canvas.draw_line(mouth.source, source, color, width)
 		for tributary in tributaries:
 			tributary.draw(canvas, color)
 	
-	func make_tributaries():
+	func make_tributaries(angle_change):
 		if width <= MIN_WIDTH:
 			return
-		# How many tributaries?
-		if randf() < EXTENSION_CHANCE:
-			# Get new angle, get new length, add 1 new tributary, move on
-			_make_tributary(width - MIN_WIDTH, ANGLE_CHANGE)
-		else:
-			# Split width in 2
-			var new_width = rand_range(MIN_WIDTH, width - MIN_WIDTH)
-			_make_tributary(new_width, ANGLE_CHANGE * 2)
-			_make_tributary(width - new_width, ANGLE_CHANGE * 2)
+
+		# Split width in 2
+		var new_width = rand_range(MIN_WIDTH, width - MIN_WIDTH)
+		_make_tributary(new_width, -angle_change)
+		_make_tributary(width - new_width, angle_change)
 			
 	func _make_tributary(new_width : float, angle_change : float):
-			var new_angle : float = get_angle() + rand_range(-angle_change, angle_change)
-			var direction : Vector2 = Vector2(new_width * LEN_WIDTH_RATIO, 0.0).rotated(new_angle) 
-			var new_section : Section = Section.new(new_width, source + direction, self)
+			var new_angle : float = angle + angle_change
+			# Bigger the angle change, shorter the section
+			var section_len = new_width * LEN_WIDTH_RATIO
+			var direction : Vector2 = Vector2(section_len, 0.0).rotated(new_angle) 
+			var new_section : Section = Section.new(new_width, new_angle, source + direction, self)
 			tributaries.append(new_section)
-			new_section.make_tributaries()
+			new_section.make_tributaries(angle_change / 2.0)
 		
 
 func _ready():
-	var off_map := Section.new(START_WIDTH, Vector2(-1.0, height / 2), null)
-	river_mouth = Section.new(START_WIDTH, Vector2(0.0, height / 2), off_map)
+	river_mouth = Section.new(START_WIDTH, START_ANGLE, Vector2(0.0, height / 2), null)
 	
 	seed(1)
-	river_mouth.make_tributaries()
+	river_mouth.make_tributaries(START_ANGLE_CHANGE)
 
 func _draw():
 	# Take advantage of the inherited CanvasItem capability
