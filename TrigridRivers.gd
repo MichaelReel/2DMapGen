b5ee60386aa37aebf1764273ca08d0972353e845
Michael Reel <mykilr@gmail.com>
2020-05-25 17:57:17 +0100

    Non-random point selection, fun but daft.

--- TrigridRivers.gd
+++ TrigridRivers.gd
@@ -1,6 +1,8 @@
 extends TextureRect
 
 const TRI_SIDE : float = 50.0
+const MIN_DEPTH : float = 1.0
+const DEPTH_CHANGE : float = 1.0
 
 onready var width := rect_size.x
 onready var height := rect_size.y
@@ -12,6 +14,8 @@ class TriPoint:
 	var col : int
 	var row : int
 	var connections : Array
+	var downstream : TriPoint
+	var depth : float
 	
 	func _init(v, x, y):
 		pos = v
@@ -43,7 +47,7 @@ class TriGrid:
 			var col_ind : int = 0
 			for x in range (offset, width, TRI_SIDE):
 				# Create point in grid
-				var new_point = TriPoint.new(Vector2(x, y), row_ind, col_ind)
+				var new_point = TriPoint.new(Vector2(x, y), col_ind, row_ind)
 				points_row.append(new_point)
 				
 				# Form connections
@@ -64,9 +68,23 @@ class TriGrid:
 			#next row
 			row_ind += 1
 	
-	func flow_to(col : int, row : int, depth : float):
+	func flow_to(col : int, row : int, depth : float, downstream = null):
 		# TODO: Recursive algorithm to visit unvisited points via connections
-		pass
+		var point = points[row][col]
+		point.depth = depth
+		if downstream != null:
+			point.downstream = downstream
+		else:
+			point.downstream = point # Sink
+		# Proportion a part of the depth to each available connection
+		if depth < MIN_DEPTH:
+			return
+#		var available = []
+		for conn in point.connections:
+			if conn.downstream == null:
+#				available.append(conn)
+				flow_to(conn.col, conn.row, depth - DEPTH_CHANGE, point)
+				
 	
 	func draw_guides(canvas : CanvasItem, color : Color):
 		for row_ind in range (0, points.size()):
@@ -75,6 +93,15 @@ class TriGrid:
 				canvas.draw_circle(point.pos, 2.0, color)
 				for conn in point.connections:
 					canvas.draw_line(point.pos, conn.pos, color, 0.5)
+					
+	func draw_flows(canvas : CanvasItem, color : Color):
+		for row_ind in range (0, points.size()):
+			for col_ind in range (0, points[row_ind].size()):
+				var point = points[row_ind][col_ind]
+				canvas.draw_circle(point.pos, 2.0, color)
+				# TODO: Draw downstream connections!
+				if point.downstream != null:
+					canvas.draw_line(point.pos, point.downstream.pos, color, point.depth)
 
 
 func _ready():
@@ -90,3 +117,4 @@ func _ready():
 	
 func _draw():
 	grid.draw_guides(self, Color(1.0, 1.0, 1.0, 0.1))
+	grid.draw_flows(self, Color(0.0, 0.5, 1.0, 1.0))
