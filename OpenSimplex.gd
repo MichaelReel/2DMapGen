f7393f291fca740ca9a7ef25db1224150c355e8d
Michael Reel <mykilr@gmail.com>
2020-06-03 19:30:45 +0100

    Simplied the OpenSimplex example down to a basic template.

--- OpenSimplex.gd
+++ OpenSimplex.gd
@@ -1,23 +1,10 @@
 extends TextureRect
 
+onready var width : int = rect_size.x
+onready var height : int = rect_size.y
+
 func _ready():
 	var imageTexture := ImageTexture.new()
 	var noise : OpenSimplexNoise = OpenSimplexNoise.new()
-	var width : int = -self.margin_right
-	var height : int = -self.margin_bottom
-
-	# Configure noise
-	noise.seed = randi()
-	noise.octaves = 4
-	noise.period = 200.0
-	noise.persistence = 1.1
-
-	var noiseImage : Image = noise.get_image(-self.margin_right, -self.margin_bottom)
-
-	imageTexture.create_from_image(noiseImage)
-	self.texture = imageTexture
-	
-	imageTexture.resource_name = "The created texture!"
-	print(self.texture.resource_name)
-	
-	pass
+	imageTexture.create_from_image(noise.get_image(width, height))
+	texture = imageTexture
